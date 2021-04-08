1d86022c5c928afcb714f1a01083e1281eed73dd
lisavader <lisa@mmvinder.nl>
2021-04-08 11:36:29 +0200

    Delete scripts/23_03_2021 directory

--- scripts/23_03_2021/bactofidia_all_reads.sh
+++
@@ -1,4 +0,0 @@
-cd ~/data/bactofidia_ST131
-ln -s ~/data/raw_reads/3rd_run/* .
-ln -s ~/data/raw_reads/st131_confirmed/* .
-sbatch -J bactofidia_ST131_all_reads --time 96:00:00 --mem 40G -c 8 ./bactofidia.sh *.fastq.gz
