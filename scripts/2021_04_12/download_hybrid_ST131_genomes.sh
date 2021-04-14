5eec808d15e962408d0572937d2d62312780358b
Lisa Vader <lisa@mmvinder.nl>
2021-04-14 12:32:52 +0200

    add search terms used

--- scripts/2021_04_12/download_hybrid_ST131_genomes.sh
+++ scripts/2021_04_12/download_hybrid_ST131_genomes.sh
@@ -7,7 +7,7 @@ conda activate ncbi_download_mmbioit
 
 #I searched for all E. coli hybrid assemblies on the https://www.ncbi.nlm.nih.gov/ Assembly database.
 #I used the following search terms:
-
+#(gridion[Sequencing Technology] OR pacbio[Sequencing Technology] OR pacific[Sequencing Technology] OR minion[Sequencing Technology] OR nanopore[Sequencing Technology] OR sequel[Sequencing Technology]) AND illumina[Sequencing Technology] AND ("Escherichia coli"[Organism] OR Escherichia coli[All Fields]) AND (latest[filter] AND "complete genome"[filter] AND all[filter] NOT anomalous[filter])
 #And saved the accessions in a file called 2021_04_12_hybrid_ecoli_accessions.
 
 #To download their sequences in fasta format:
@@ -20,10 +20,8 @@ esearch -db assembly -query ${strain} | elink -target nuccore | efetch -format f
 done
 
 #Running mlst:
-
 mlst genomes/* > mlst_output.tsv
 
 #Some genomes were downloaded in duplicate, creating a double allelic output in the mlst table, eg. adk(53,53) instead of adk(53).
 #Because strains containing multiple alleles are by default not assigned a ST, I used the following code to select all ST131 strains:
-
 cat mlst_output.tsv | grep adk.53.*fumC.*40.*gyrB.47.*icd.13.*mdh.*36.*purA.28.*recA.*29.* | cut -f 1 > hybrid_ST131_accessions
