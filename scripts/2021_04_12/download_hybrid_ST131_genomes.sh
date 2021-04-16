e47c1edce6a418323919dd0beab45e2ed498e634
Lisa Vader <lisa@mmvinder.nl>
2021-04-16 12:01:06 +0200

    update fasta download

--- scripts/2021_04_12/download_hybrid_ST131_genomes.sh
+++ scripts/2021_04_12/download_hybrid_ST131_genomes.sh
@@ -5,26 +5,30 @@ cd ~/data/scripts/06_04_2021/
 conda env create -f ncbi_download_mmbioit.yml
 conda activate ncbi_download_mmbioit
 
-#I searched for all E. coli hybrid assemblies on the https://www.ncbi.nlm.nih.gov/ Assembly database.
+#I searched for all E. coli long read assemblies in the https://www.ncbi.nlm.nih.gov/ Assembly database.
 #I used the following search terms:
-#(gridion[Sequencing Technology] OR pacbio[Sequencing Technology] OR pacific[Sequencing Technology] OR minion[Sequencing Technology] OR nanopore[Sequencing Technology] OR sequel[Sequencing Technology]) AND illumina[Sequencing Technology] AND ("Escherichia coli"[Organism] OR Escherichia coli[All Fields]) AND (latest[filter] AND "complete genome"[filter] AND all[filter] NOT anomalous[filter])
-#And saved the accessions in a file called 2021_04_12_hybrid_ecoli_accessions.
+#(hybrid[Sequencing Technology] OR gridion[Sequencing Technology] OR pacbio[Sequencing Technology] OR pacific[Sequencing Technology] OR minion[Sequencing Technology] OR nanopore[Sequencing Technology] OR sequel[Sequencing Technology]) AND ("Escherichia coli"[Organism] OR Escherichia coli[All Fields]) AND (latest[filter] AND "complete genome"[filter] AND all[filter] NOT anomalous[filter])
+#And saved the accessions (send to file: ID Table) in a file called 2021_04_15_longread_ecoli_list.
 
-#To download their sequences in fasta format:
-cd ~/data/genome_download
-accessions=$(cat 2021_04_12_hybrid_ecoli_accessions)
-
-for strain in $accessions
+#To get their accessions and ftp paths:
+cat 2021_04_15_longread_ecoli_list.txt | cut -f 1 | sed '1d' | sort > 2021_04_15_longread_ecoli_accessions
+for accession in $accessions
 do
-esearch -db assembly -query ${strain} | elink -target nuccore | efetch -format fasta  > genomes/${strain}.fna
+esearch -db assembly -query ${accession} | esummary | xmllint --xpath "string(//FtpPath[@type='GenBank'])" - >> 2021_04_15_longread_ecoli_ftppaths
+done
+
+#Downloading the sequences in fasta format:
+urls=$(cat 2021_04_15_longread_ecoli_ftppaths)
+cd ~/data/genome_download/genomes
+
+for url in $urls
+do
+name=$(cut -d / -f 10 <<<${url})
+wget ${url}/${name}_genomic.fna.gz
 done
 
 #Running mlst:
 mlst genomes/* > mlst_output.tsv
-
-#Some genomes were downloaded in duplicate, creating a double allelic output in the mlst table, eg. adk(53,53) instead of adk(53).
-#Because strains containing multiple alleles are by default not assigned a ST, I used the following code to select all ST131 strains:
-cat mlst_output.tsv | grep adk.53.*fumC.*40.*gyrB.47.*icd.13.*mdh.*36.*purA.28.*recA.*29.* | cut -f 1 | cut -c 9-23 > hybrid_ST131_accessions
 
 #To get the biosamples that belong to the assembly accessions:
 accessions=$(cat hybrid_ST131_accessions)
