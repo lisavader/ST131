48254725be8151d46f2c27bb36107b2f59d1ee74
Lisa Vader <lisa@mmvinder.nl>
2021-05-31 16:01:30 +0200

    add comments and change file links

--- ST131_ncbi_download/scripts/accessions_table.sh
+++ ST131_ncbi_download/scripts/accessions_table.sh
@@ -1,7 +1,13 @@
-sra_accessions=$(cat ~/data/genome_download/longread_ST131_sra_accessions)
+#activate conda environment
+source /home/dla_mm/lvader/data/miniconda3/etc/profile.d/conda.sh
+conda activate ncbi_download_mmbioit
 
+sra_accessions=$(cat ../results/longread_ST131_sra_accessions)
+
+#build table with column names
 echo "Assembly Accession,SRA Accession" > accessions_table.csv
 
+#for each sra accession, find the corresponding assembly accession using entrez utilities and append to table
 for sra_accession in $sra_accessions
 do
 assembly_accession=$(esearch -db sra -query ${sra_accession} | elink -target biosample | elink -target assembly | esummary | xmllint --xpath "string(//Genbank)" -)
