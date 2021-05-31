2678be35811c9d9ec04e7b9cb5377448289e6db8
Lisa Vader <lisa@mmvinder.nl>
2021-05-31 18:04:04 +0200

    add variable for directory name

--- ST131_ncbi_download/scripts/run_busco.sh
+++ ST131_ncbi_download/scripts/run_busco.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#define directory name
+assemblies_dir=shortread_assemblies_unicycler
+
+cd ../results
+
 mkdir busco_output
 cd busco_output
 
-accessions=$(ls ../assemblies)
+accessions=$(ls $assemblies_dir)
 
 for acc in ${accessions}
 do
-busco -i ../assemblies/${acc}/assembly.fasta -l enterobacterales_odb10 -o ${acc} -m genome
+busco -i ../$assemblies_dir/${acc}/assembly.fasta -l enterobacterales_odb10 -o ${acc} -m genome
 done
