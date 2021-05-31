7bfbb6ff11a45e6d1d6b827f6d89c0b3421ccfd0
Lisa Vader <lisa@mmvinder.nl>
2021-05-31 18:08:59 +0200

    change output directory name and path

--- ST131_ncbi_download/scripts/unicycler.sh
+++ ST131_ncbi_download/scripts/unicycler.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd ~/data/genome_download/
+cd ../results
+
+mkdir shortread_assemblies_unicycler
+
 sra_accessions=$(cat longread_ST131_sra_accessions)
 
 for accession in $sra_accessions
 do
-unicycler --threads 20 -1 sra_files/${accession}_1.fastq -2 sra_files/${accession}_2.fastq -o ~/data/mobsuite_test/assemblies/${accession}
+unicycler --threads 20 -1 sra_files/${accession}_1.fastq -2 sra_files/${accession}_2.fastq -o shortread_assemblies_unicycler/${accession}
 done
