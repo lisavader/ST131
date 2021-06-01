d50f9b78bd6d76a888443215920f5a2d0c6849cd
Lisa Vader <lisa@mmvinder.nl>
2021-06-01 11:06:55 +0200

    change paths

--- benchmarking/plasmidspades/scripts/add_assembly_accessions.sh
+++ benchmarking/plasmidspades/scripts/add_assembly_accessions.sh
@@ -1,11 +1,11 @@
 
-cd quast_output
+cd ../results/quast_output
 
 #for each line in the statistics file
 cat spades_alignments_statistics.csv | while read line
 do
 sra_accession=$(echo $line | cut -d , -f 1)
-assembly_accession=$(cat ~/data/genome_download/accessions_table.csv | grep $sra_accession | cut -d , -f 1)
+assembly_accession=$(cat ../../../../ST131_ncbi_download/results/accessions_table.csv | grep $sra_accession | cut -d , -f 1)
 echo $assembly_accession,$line >> outfile.csv		#new file has a first column with assembly accessions
 done
 
