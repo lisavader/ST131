e91c18854b56474034a8aecee02627ea005c6ea8
Lisa Vader <lisa@mmvinder.nl>
2021-06-01 15:52:00 +0200

    update paths

--- benchmarking/mobsuite/scripts/mobsuite_parallel.sh
+++ benchmarking/mobsuite/scripts/mobsuite_parallel.sh
@@ -1,7 +1,7 @@
 #Author: Julian Paganini
 #Adjusted by Lisa Vader
 
-cd ~/data/mobsuite_test
+cd ../results
 
 source /home/dla_mm/lvader/data/miniconda3/etc/profile.d/conda.sh
 conda activate mobsuite
@@ -13,7 +13,7 @@ mkdir mob_sbatch_scripts
 mkdir mob_predictions
 
 #3. Get a list of strain names
-accessions=$(cat ~/data/genome_download/longread_ST131_sra_accessions)
+accessions=$(cat ../../../ST131_ncbi_download/results/longread_ST131_sra_accessions)
 
 
 #4- create sbatch scripts - this part will create one individual sbatch script for running MOB for the samples
@@ -21,9 +21,9 @@ for strain in $accessions
 do
 echo "#!/bin/bash
 #1. Move to the directory that will contain the mob predictions
-cd ~/data/mobsuite_test/mob_predictions
+cd ../mob_predictions
 #2. Run MOB-suite
-mob_recon --infile ~/data/mobsuite_test/assemblies/${strain}/assembly.fasta --outdir ${strain}" > mob_sbatch_scripts/${strain}.sh
+mob_recon --infile ../../../../ST131_ncbi_download/results/shortread_assemblies_unicycler/${strain}/assembly.fasta --outdir ${strain}" > mob_sbatch_scripts/${strain}.sh
 done
 
 #5- Run the sbatch scripts
