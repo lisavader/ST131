d95551eb6f74f0f04cb974969861e152d67dddd1
Lisa Vader <lisa@mmvinder.nl>
2021-06-01 15:45:40 +0200

    update paths

--- benchmarking/mobsuite/scripts/add_assembly_accessions_mas.sh
+++ benchmarking/mobsuite/scripts/add_assembly_accessions_mas.sh
@@ -1,8 +1,9 @@
+cd ../results/quast_output
 
 cat mob_alignments_statistics.csv | while read line
 do
 sra_accession=$(echo $line | cut -d , -f 1)
-assembly_accession=$(cat ../accessions_table.csv | grep $sra_accession | cut -d , -f 1)
+assembly_accession=$(cat ../../../../ST131_ncbi_download/results/accessions_table.csv | grep $sra_accession | cut -d , -f 1)
 echo $assembly_accession,$line >> outfile.csv
 done
 
--- benchmarking/mobsuite/scripts/gather_quast_results.py
+++ benchmarking/mobsuite/scripts/gather_quast_results.py
@@ -634,8 +634,8 @@ def bin_status(strain):
                 
 #directories paths
 wd=os.path.dirname(os.path.realpath(__file__))
-predictions_directory='mob_predictions/'
-alignment_directory='quast_output/'
+predictions_directory='../results/mob_predictions/'
+alignment_directory='../results/quast_output/'
 
 os.chdir(predictions_directory) #this will have to change for every software
 genomes=glob.glob('?RR*') #this will have to change for every software
