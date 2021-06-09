d9c61062044144a970559ea96a62db016f7d31cc
Lisa Vader <lisa@mmvinder.nl>
2021-06-09 16:14:49 +0200

    run mlplasmids from the hpc

---
+++ benchmarking/mlplasmids/scripts/run_mlplasmids.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##create output directory
+mkdir ../results/mlplasmids_predictions
+
+##clone mlplasmids
+cd ~/data #provide home directory
+#git clone https://gitlab.com/sirarredondo/mlplasmids.git
+cd mlplasmids
+
+##run mlplasmids
+for file in ../ST131_repo/ST131_ncbi_download/results/shortread_assemblies_bactofidia/scaffolds/*.fna
+do
+name=$(basename $file .fna)
+Rscript scripts/run_mlplasmids.R $file ../ST131_repo/benchmarking/mlplasmids/results/mlplasmids_predictions/${name}_0.5.tsv 0.5 'Escherichia coli'
+done
