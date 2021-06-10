90f32c6990f95af2791f0f39d850fb85db2b3403
Lisa Vader <lisa@mmvinder.nl>
2021-06-10 11:49:50 +0200

    set threshold to very small number so that all contigs are reported

--- benchmarking/mlplasmids/scripts/run_mlplasmids.sh
+++ benchmarking/mlplasmids/scripts/run_mlplasmids.sh
@@ -12,5 +12,5 @@ cd mlplasmids
 for file in ../ST131_repo/ST131_ncbi_download/results/shortread_assemblies_bactofidia/scaffolds/*.fna
 do
 name=$(basename $file .fna)
-Rscript scripts/run_mlplasmids.R $file ../ST131_repo/benchmarking/mlplasmids/results/mlplasmids_predictions/${name}_0.5.tsv 0.5 'Escherichia coli'
+Rscript scripts/run_mlplasmids.R $file ../ST131_repo/benchmarking/mlplasmids/results/mlplasmids_predictions/${name}.tsv 1e-5 'Escherichia coli'
 done
