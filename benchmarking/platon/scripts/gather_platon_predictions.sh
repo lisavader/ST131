9b5a7e0bef9b35d12a26e4ec74af8552f45efdf5
Lisa Vader <lisa@mmvinder.nl>
2021-06-14 14:03:48 +0200

    change extension output file to csv

--- benchmarking/platon/scripts/gather_platon_predictions.sh
+++ benchmarking/platon/scripts/gather_platon_predictions.sh
@@ -6,11 +6,11 @@ do
 cd $file
 cat *chromosome.fasta | grep '>' | while read line
 do
-echo $line,"chromosome" >> ../../platon_all_results.tsv
+echo $line,"chromosome" >> ../../platon_all_results.csv
 done
 cat *plasmid.fasta | grep '>' | while read line
 do
-echo $line,"plasmid" >> ../../platon_all_results.tsv
+echo $line,"plasmid" >> ../../platon_all_results.csv
 done
 cd ..
 done
