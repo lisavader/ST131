718876b6e3124b67149ac39a5da35536d0c51c75
Lisa Vader <lisa@mmvinder.nl>
2021-07-02 10:45:37 +0200

    extract lines containing best group = True from quast output

--- ST131_ncbi_download/scripts/gather_contigs_replicons.sh
+++ ST131_ncbi_download/scripts/gather_contigs_replicons.sh
@@ -1,9 +1,9 @@
-
-cd ../results/quast_output_bactofidia_contigs
+#put all the quast all_alignments output files together and extract true lines
+cd ../results/quast_output_bactofidia_contigs/ambiguity_all/ 
 
 strains=$(ls)
 
 for strain in $strains
 do
-cat ${strain}/contigs_reports/all_alignments_*.tsv | grep -v CONTIG | cut -f 5,6 | grep cov | uniq >> ../bactofidia_contigs_mapped.tsv
+cat ${strain}/contigs_reports/all_alignments_*.tsv | grep 'True' >> ../../bactofidia_all_true_alignments.tsv
 done
