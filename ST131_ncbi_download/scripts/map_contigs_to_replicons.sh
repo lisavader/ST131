3288f6e9e4f3b236b6636a89c15fd4042f226228
Lisa Vader <lisa@mmvinder.nl>
2021-07-05 15:47:02 +0200

    use ambiguity_all quast results

--- ST131_ncbi_download/scripts/map_contigs_to_replicons.sh
+++ ST131_ncbi_download/scripts/map_contigs_to_replicons.sh
@@ -7,12 +7,12 @@ conda activate quast
 cd ../results
 
 #make directory for storing quast results
-mkdir quast_output_bactofidia_contigs
+mkdir quast_output_bactofidia_contigs/ambiguity_all
 
 #find assembly accession and run quast with assembly as reference
 for file in shortread_assemblies_bactofidia/scaffolds/*.fna
 do
 name=$(basename $file .fna)
 assembly_accession=$(grep $name  accessions_table.csv | cut -d , -f 1)      
-quast -o quast_output_bactofidia_contigs/${name} -r genomes/${assembly_accession}*genomic.fna -m 1000 -t 8 -i 500 --fast --ambiguity-usage one $file
+quast -o quast_output_bactofidia_contigs/ambiguity_all/${name} -r genomes/${assembly_accession}*genomic.fna -m 1000 -t 8 -i 500 --fast --ambiguity-usage all $file
 done
