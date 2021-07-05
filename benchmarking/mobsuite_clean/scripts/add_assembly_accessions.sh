86e03f52b390942cf8788f4123095ad6dc0e0dc1
Lisa Vader <lisa@mmvinder.nl>
2021-07-05 15:53:31 +0200

    copied from mobsuite bac

---
+++ benchmarking/mobsuite_clean/scripts/add_assembly_accessions.sh
@@ -0,0 +1,14 @@
+
+cd ../results/quast_output_clean
+
+#for each line in the statistics file
+cat mob_alignments_statistics.csv | while read line
+do
+sra_accession=$(echo $line | cut -d , -f 1)
+assembly_accession=$(cat ../../../../ST131_ncbi_download/results/accessions_table.csv | grep $sra_accession | cut -d , -f 1)
+echo $assembly_accession,$line >> outfile.csv		#new file has a first column with assembly accessions
+done
+
+#replace original file with new file
+cat outfile.csv > mob_alignments_statistics.csv
+rm outfile.csv
