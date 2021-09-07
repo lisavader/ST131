f0c4191c7e5cd4ae9c85823d5bf863b3473a2d4c
Lisa Vader <lisa@mmvinder.nl>
2021-09-07 14:53:43 +0200

    make mobsuite script general

---
+++ benchmarking/plasmid_reconstruction/scripts/run_mobsuite.sh
@@ -0,0 +1,57 @@
+#Author: Julian Paganini
+#Adjusted by Lisa Vader
+
+cd ../results
+
+source /home/dla_mm/lvader/data/miniconda3/etc/profile.d/conda.sh
+conda activate mobsuite
+
+#0. set mode
+while getopts :m: flag; do
+        case $flag in
+                m) mode=$OPTARG;;
+        esac
+done
+
+#1. create directory to store slurm jobs
+rm -rf mob_scripts_$mode
+mkdir mob_scripts_$mode
+
+#2. Create directory to store mob-results
+rm -rf mob_predictions_$mode
+mkdir mob_predictions_$mode
+
+#3. Get a list of strain names
+accessions=$(cat ../../../ST131_ncbi_download/results/longread_ST131_sra_accessions)
+
+
+#4- create sbatch scripts - this part will create one individual sbatch script for running MOB for the samples
+for strain in $accessions
+do
+
+if [[ $mode = "bactofidia" ]];  then
+        path=$(echo "shortread_assemblies_bactofidia/scaffolds/${strain}.fna")
+fi
+
+if [[ $mode = "unicycler" ]];  then
+        path=$(echo "shortread_assemblies_unicycler/${strain}/assembly.fasta")
+fi
+
+if [[ $mode = "unicycler_trimmed" ]];  then
+        path=$(echo "shortread_assemblies_unicycler_trimmed/${strain}/assembly.fasta")
+fi
+
+echo "#!/bin/bash
+#1. Move to the directory that will contain the mob predictions
+cd ../mob_predictions_${mode}
+#2. Run MOB-suite
+mob_recon --infile ../../../../ST131_ncbi_download/results/${path} --outdir ${strain}" > mob_scripts_${mode}/${strain}.sh
+done
+
+#5- Run the sbatch scripts
+cd mob_scripts_${mode}
+jobs=$(ls)
+for slurm in $jobs
+do
+sbatch --time=2:00:00 --mem=5G ${slurm}
+done
