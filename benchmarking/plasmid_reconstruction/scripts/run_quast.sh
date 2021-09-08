b432594d592fb29a322e5609a0c66919cc16b74e
Lisa Vader <lisa@mmvinder.nl>
2021-09-08 10:31:41 +0200

    remove errors in slurm script

--- benchmarking/plasmid_reconstruction/scripts/run_quast.sh
+++ benchmarking/plasmid_reconstruction/scripts/run_quast.sh
@@ -41,10 +41,7 @@ cd ../..
 for bin in $all_bins
 do
 echo "#!/bin/bash
-#move to root directory
-cd ../../../..
-#perform quast
-quast -o quast_output/${sra_accession}/${bin} -r ../../../ST131_ncbi_download/results/genomes/${assembly_accession}*genomic.fna -m 1000 -t 8 -i 500 --no-snps --ambiguity-usage all predictions_${mode}/${sra_accession}/${bin}.fasta" > quast_scripts_${mode}/${sra_accession}_${bin}
+quast -o quast_${mode}/${sra_accession}/${bin} -r ../../../ST131_ncbi_download/results/genomes/${assembly_accession}*genomic.fna -m 1000 -t 8 -i 500 --no-snps --ambiguity-usage all predictions_${mode}/${sra_accession}/${bin}.fasta" > quast_scripts_${mode}/${sra_accession}_${bin}
 done
 done
 
