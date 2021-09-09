81fd1bbc1f7d1b4a73a217ead57e5fd53efb600f
Lisa Vader <lisa@mmvinder.nl>
2021-09-09 18:16:44 +0200

    knit all individual scripts together

---
+++ benchmarking/plasmid_reconstruction/scripts/main.sh
@@ -0,0 +1,42 @@
+#SBATCH -J reconstruct_plasmids
+#SBATCH --time=20:00:00
+#SBATCH --mem=32G
+#SBATCH -c 8
+
+#Set mode
+while getopts :m: flag; do
+        case $flag in
+                m) mode=$OPTARG;;
+        esac
+done
+
+#Activate conda for loading python libraries
+source /home/dla_mm/lvader/data/miniconda3/etc/profile.d/conda.sh
+conda activate mobsuite
+
+##Run all scripts
+#For parallelized scripts, I added a latency so that we only move on when the slurm scripts are finished
+
+#Run plasmid reconstruction tools
+if [[ $mode = mob* ]]; then
+	bash run_mobsuite.sh -m $mode
+	sleep 2h
+
+elif [[ $mode = spades ]]; then
+	bash run_plasmidspades.sh
+	sleep 4h 30m
+	#for plasmidspades, also separate the bins
+	python separate_spades_bins.py	
+
+#In case of the EC 'cleaned' approach, remove predicted chromosomal contigs from bins
+if [[ $mode = mob.*cleaned ]]; then
+
+#Run quast
+bash run_quast.sh -m $mode
+sleep 5m
+
+#Gather quast results
+python gather_quast_results.py $mode
+
+#Add assembly accessions to alignment statistics file
+bash add_assembly_accessions.sh -m $mode
