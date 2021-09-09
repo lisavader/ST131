2d84e5c3ef5fa1d57ba856b8e27c7d284bc4cf08
Lisa Vader <lisa@mmvinder.nl>
2021-09-09 12:20:01 +0200

    turn into general script

---
+++ benchmarking/plasmid_reconstruction/scripts/add_assembly_accessions.sh
@@ -0,0 +1,19 @@
+#set mode
+while getopts :m: flag; do
+        case $flag in
+                m) mode=$OPTARG;;
+        esac
+done
+
+#add assembly accessions to alignments statistics file
+cd ../results/quast_${mode}
+
+cat ${mode}_alignments_statistics.csv | while read line
+do
+sra_accession=$(echo $line | cut -d , -f 1)
+assembly_accession=$(cat ../../../../ST131_ncbi_download/results/accessions_table.csv | grep $sra_accession | cut -d , -f 1)
+echo $assembly_accession,$line >> outfile.csv
+done
+
+cat outfile.csv > ${mode}_alignments_statistics.csv
+rm outfile.csv
