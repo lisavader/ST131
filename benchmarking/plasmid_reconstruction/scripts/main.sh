2efa05bd88a85ef9aea62f95127d7ba8c8b32a01
Lisa Vader <lisa@mmvinder.nl>
2021-09-10 11:06:21 +0200

    add descriptive output

--- benchmarking/plasmid_reconstruction/scripts/main.sh
+++ benchmarking/plasmid_reconstruction/scripts/main.sh
@@ -19,10 +19,12 @@ conda activate mobsuite
 
 #Run plasmid reconstruction tools
 if [[ $mode = mob* ]]; then
+	echo "Running mobsuite..."
 	bash run_mobsuite.sh -m $mode
 	sleep 2h
 
 elif [[ $mode = spades ]]; then
+	echo "Running plasmidspades..."
 	bash run_plasmidspades.sh
 	sleep 4h 30m
 	#for plasmidspades, also separate the bins
@@ -31,17 +33,21 @@ fi
 
 #In case of the EC 'cleaned' approach, remove predicted chromosomal contigs from bins
 if [[ $mode = mob.*cleaned ]]; then
+	echo "Removing chromosomal contamination from bins..."
 	python remove_contamination.py $mode
 fi
 
 #Run quast
+echo "Running quast..."
 bash run_quast.sh -m $mode
 sleep 5m
 
 #Gather quast results
+echo "Gathering quast results..."
 python gather_quast_results.py $mode
 
 #Add assembly accessions to alignment statistics file
+echo "Adding assembly accessions..."
 bash add_assembly_accessions.sh -m $mode
 
 #Remove slurm scripts folders
