a19a292249b400f2139686faed0394a2ef6e032e
Lisa Vader <lisa@mmvinder.nl>
2021-09-10 13:06:32 +0200

    general script for selecting plasmid contigs before running mobsuite, in progress

---
+++ benchmarking/plasmid_reconstruction/scripts/select_plasmid_contigs.py
@@ -0,0 +1,58 @@
+#import libraries
+import fastaparser
+import glob
+import os
+import sys
+
+#set mode and assign mode-specific variables
+mode=str(sys.argv[1])
+
+if "uni" in mode:
+	assemblies_dir='../../../ST131_ncbi_download/results/shortread_assemblies_unicycler/'
+	EC_prediction_path='../../../benchmarking/analysis/results/EC_result_all_contigs3.csv'
+elif "bac" in mode:
+	assemblies_dir='../../../ST131_ncbi_download/results/shortread_assemblies_bactofidia/scaffolds/'
+	EC_prediction_path='../../../../benchmarking/analysis/results/EC_result_all_contigs2.csv'
+
+#Select plasmid contigs
+#INPUT: name of assembly file
+#OUTPUT: assembly file containing only contigs predicted as plasmids by the ensemble classifier
+def select_plasmid_contigs(strain):
+	if "uni" in mode:
+		assembly_file=strain+'/assembly.fasta'
+	elif "bac" in mode:
+		assembly_file=strain
+	with open(assembly_file,'r') as fasta:
+		assembly = fastaparser.Reader(fasta)
+		for contig in assembly:
+			#for unicycler, merge strain with contig id to obtain contig name
+			if "uni" in mode:
+				contig_name=strain+'_'+contig.id
+			elif "bac" in mode:
+				contig_name=contig.id
+			if contig_name in plasmids:
+				with open("predicted_plasmid_contigs/"+strain,'a') as output:
+					writer = fastaparser.Writer(output)
+					writer.writefasta(contig)
+
+
+#go to assemblies directory
+os.chdir(assemblies_dir)
+
+#make empty list
+plasmids = []
+
+#save contig headers of predicted plasmids in list
+with open(EC_prediction_path,'r') as EC_result:
+	for line in EC_result.readlines():
+		if 'plasmid' in line:
+			contig_name=line.split(',')[0].strip('"')
+			plasmids.append(contig_name)
+
+#extract and save plasmid contigs for each assembly file
+strains=glob.glob('?RR*')
+for strain in strains:
+	with open("predicted_plasmid_contigs/"+strain,'w') as output:
+		pass
+	select_plasmid_contigs(strain)
+	
