8ca01f51e67a8d608bf370b7759f0605154a8153
Lisa Vader <lisa@mmvinder.nl>
2021-09-14 11:12:42 +0200

    update directory structure

--- benchmarking/plasmid_reconstruction/scripts/add_assembly_accessions.sh
+++ benchmarking/plasmid_reconstruction/scripts/add_assembly_accessions.sh
@@ -6,12 +6,12 @@ while getopts :m: flag; do
 done
 
 #add assembly accessions to alignments statistics file
-cd ../results/quast_statistics
+cd ../results/quast_statistics/${mode}
 
 cat ${mode}_alignments_statistics.csv | while read line
 do
 sra_accession=$(echo $line | cut -d , -f 1)
-assembly_accession=$(cat ../../../../ST131_ncbi_download/results/accessions_table.csv | grep $sra_accession | cut -d , -f 1)
+assembly_accession=$(cat ../../../../../ST131_ncbi_download/results/accessions_table.csv | grep $sra_accession | cut -d , -f 1)
 echo $assembly_accession,$line >> outfile.csv
 done
 
