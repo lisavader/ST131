a28749a6118e1f77b7f2f21d2129c70865a6555d
Lisa Vader <lisa@mmvinder.nl>
2021-09-21 17:08:59 +0200

    change strain names back to original

--- Ecoli_ncbi_download/scripts/run_bactofidia.sh
+++ Ecoli_ncbi_download/scripts/run_bactofidia.sh
@@ -3,6 +3,16 @@
 #SBATCH --mem=40G
 #SBATCH -c 8
 
+#run_bactofidia
 cd ../../../bactofidia
 ln -s ../ST131_repo/Ecoli_ncbi_download/results/raw_reads/* .
 bash ./bactofidia.sh *.fastq.gz
+
+#move output to folder
+
+#change names back
+cd ../ST131_repo/Ecoli_ncbi_download/results/shortread_assemblies_bactofidia/scaffolds
+for strain in $(ls *.fna); do 
+	new_name=$(echo $strain | sed 's/-/_/g')
+	mv $strain $new_name 
+done
