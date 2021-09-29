b990b7f8617f093e30b6736f1c760c18901a7225
Lisa Vader <lisa@mmvinder.nl>
2021-09-29 17:58:07 +0200

    run in parallel for unicycler

--- benchmarking/binary_classifiers/scripts/run_rfplasmid.sh
+++ benchmarking/binary_classifiers/scripts/run_rfplasmid.sh
@@ -2,6 +2,7 @@
 
 #create output directory
 mkdir -p ../results/rfplasmid_predictions
+mkdir -p ../results/rfplasmid_scripts
 
 #activate conda
 source /home/dla_mm/lvader/data/miniconda3/etc/profile.d/conda.sh
@@ -9,10 +10,15 @@ conda activate rfplasmid
 
 run_rfplasmid(){
 cd ../results/rfplasmid_predictions
+for strain in ../../$1; do
 #check whether input directory exists
-[ ! -d ../../$1 ] && exit 1
+[ ! -d $strain ] && exit 1
+name=$(basename $strain)
 #run rfplasmid
-rfplasmid --species Enterobacteriaceae --input ../../$1 --jelly --threads 8 --out .
+echo "#!/bin/bash
+cd ../rfplasmid_predictions
+rfplasmid --species Enterobacteriaceae --input $strain --jelly --threads 8 --out $name" > ../rfplasmid_scripts/${name}.sh
+done
 }
 
 while getopts :i: flag; do
