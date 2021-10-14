59f4cc6cfc92b096e4bd866cec614abe6213c933
Lisa Vader <lisa@mmvinder.nl>
2021-10-14 15:10:21 +0200

    use functions, add combine results script

--- rgnosis_samples/scripts/EC_main.sh
+++ rgnosis_samples/scripts/EC_main.sh
@@ -7,11 +7,28 @@ path=$(echo 'results/bactofidia_output_ST131/scaffolds')
 output_file=$(echo 'EC_output.csv')
 
 #run binary classifiers
-for tool in rfplasmid; do
+run_binary_classifiers(){
+for tool in plascope platon rfplasmid; do
 sbatch -c 8 --mem 50G --time 4:00:00 --gres=tmpspace:50G ../../benchmarking/binary_classifiers/scripts/run_${tool}.sh -i $path
 done
+}
 
 #gather results
-#sleep 1h
-#bash ../../benchmarking/binary_classifiers/scripts/gather_results.sh -o ../results/$output_file
+gather_results(){
+bash ../../benchmarking/binary_classifiers/scripts/gather_results.sh -o ../results/$output_file
+}
 
+#combine results
+combine_results(){
+Rscript ../../benchmarking/binary_classifiers/scripts/combine_results.R
+}
+
+#Use this result to select all plasmid contigs
+select_plasmid_contigs(){
+source /home/dla_mm/lvader/data/miniconda3/etc/profile.d/conda.sh
+conda activate mobsuite
+python ../../benchmarking/plasmid_reconstruction/scripts/select_plasmid_contigs.py dataset rgnosis
+}
+
+#specify which parts to run:
+select_plasmid_contigs
