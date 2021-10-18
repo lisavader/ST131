5e963a9061300c20ae82782c248132aedb8a8484
Lisa Vader <lisa@mmvinder.nl>
2021-10-18 17:32:50 +0200

    Add treatment column, plot data, extract metadata for analyses

--- rgnosis_metadata/scripts/get_ST131_metadata.R
+++ rgnosis_metadata/scripts/get_ST131_metadata.R
@@ -1,9 +1,9 @@
 
-require(readr)
-require(readxl)
-require(magrittr)
-require(dplyr)
-require(stringr)
+packages=c("readr","readxl","magrittr","dplyr","stringr","xtable","tidyr","ggplot2","tmaptools")
+for (package in packages){
+  if (!require(package, character.only = TRUE)) install.packages(package)
+  require(package, character.only = TRUE)
+}
 
 setwd("T:/microbiologie/Sequencing/Bio-Informatica/Lisa_Vader/")
 
@@ -30,7 +30,63 @@ all_IDs <- bind_rows(WGS_IDs,Julians_IDs)
 
 #Get ST131 metadata
 ST131_IDs <- inner_join(all_IDs,ST131_assembly_summaries,by="run_ID") %>% select(sample_ID,run_ID) %>% filter(sample_ID!='NA')
-ST131_metadata <- inner_join(ST131_IDs,rgnosis_metadata) %>% select(sample_ID,run_ID,subject,SITE_N,samp_per,tracti,everything())
+ST131_metadata <- inner_join(ST131_IDs,rgnosis_metadata) %>% select(sample_ID,run_ID,subject,SITE_N,stud_per,samp_per,INCL,tracti,ICU_LOS,sample_date,adm_date_icu,everything())
+
+#Find samples for which study_per and samp_per do not match
+nonmatching_periods <- ST131_metadata %>% filter(stud_per!=samp_per) %>% select(sample_ID,stud_per,samp_per,SITE_N)
+write.table(nonmatching_periods,"nonmatching_periods.txt",sep = "\t",row.names = FALSE)
+
+#Add treatment column
+ST131_metadata %<>% mutate(treatment=stud_per)%>% select(1:4,treatment,everything())
+#make baseline if interruption or before study start
+baseline_treatments <- c('baseline period','INTERRUPTION','before study start')
+ST131_metadata %<>% mutate(treatment = ifelse(stud_per %in% baseline_treatments, 'baseline', treatment))
+#make baseline if INCL = no
+ST131_metadata %<>% mutate(treatment = ifelse(INCL == 'no', 'baseline', treatment))
 
 #Make a nice table
-table(ST131_metadata$SITE_N,ST131_metadata$samp_per)
+hospital_distribution <- table(ST131_metadata$SITE_N,ST131_metadata$treatment)
+write.table(hospital_distribution,"hospital_distribution.txt",sep = "\t")
+
+#summarise metadata
+summary <- ST131_metadata %>% select(SITE_N,stud_per,INCL)
+
+#Export metadata file
+write.csv(ST131_metadata,"data/metadata/ST131_metadata_20210521.csv",row.names = FALSE)
+
+##Plotting
+theme_set(theme_bw())
+
+#Days at ICU
+ggplot(ST131_metadata,aes(x=ICU_LOS, fill=INCL)) +
+  geom_histogram(binwidth = 5,breaks = seq(0,85,by=5),colour='black')+
+  scale_x_continuous(breaks= seq(0,85,by=10))+
+  scale_y_continuous(breaks= seq(0,20,by=5),minor_breaks = seq(0,20,by=1))+
+  labs
+
+#Days at ICU when sampled
+ST131_metadata %<>% mutate(LOS_sampled=as.Date(sample_date)-as.Date(adm_date_icu))
+ST131_metadata %<>% select(1:12,LOS_sampled,everything())
+
+#exclude weird sample in row 76
+ggplot(ST131_metadata[-76,],aes(x=LOS_sampled, fill=INCL)) +
+  geom_histogram(binwidth = 1,colour="black")+
+  scale_x_continuous(breaks= seq(0,60,by=10))+
+  scale_y_continuous(breaks= seq(0,20,by=5),minor_breaks = seq(0,20,by=1))
+
+#fun plot of both
+ggplot(ST131_metadata[-76,],aes(x=ICU_LOS,y=LOS_sampled))+
+  geom_point()+
+  geom_abline()
+#what's up with 63338?
+
+#select metadata for general analysis
+ST131_metadata_selected <- ST131_metadata %>% select(run_ID,treatment,SITE_N,tracti)
+write.csv(ST131_metadata_selected,"selected_ST131_metadata.csv",row.names = FALSE)
+
+#select metadata for microreact
+ST131_metadata_microreact <- ST131_metadata %>% rename(id=run_ID,year=sample_yr,month=sample_mo,day=sample_day) %>% select(id,treatment,SITE_N,tracti,year,month,day)
+write.csv(ST131_metadata_microreact,"ST131_metadata_microreact.csv",row.names = FALSE)
+
+#geocode with tmap
+hospital_coordinates <- geocode_OSM(ST131_metadata$SITE_N)
