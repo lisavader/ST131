82d5989f1bd5b342b963d59ab826b35a19d1227d
Lisa Vader <lisa@mmvinder.nl>
2021-11-11 14:01:38 +0100

    install and run clermontyping

---
+++ rgnosis_samples/scripts/clermontyping.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source ~/data/miniconda3/etc/profile.d/conda.sh
+
+build_environment(){
+conda create --name clermontyping
+conda activate cermontyping
+conda install -c kantorlab blastn
+conda install -c anaconda biopython
+conda install -c conda-forge/label/cf201901 pandoc
+conda install -c conda-forge r-readr r-dplyr r-tidyr r-stringr r-knitr r-rmarkdown
+}
+
+clone_clermontyping(){
+cd ../tools
+git clone https://github.com/A-BN/ClermonTyping clermontyping
+}
+
+run_clermontyping(){
+cd ../tools/clermontyping
+#soft link fasta files
+#ln -s ../../results/bactofidia_output_all/scaffolds/*.fasta .
+#build input string of fasta files separated by a @ character
+fasta_files=$(ls -1 *.fasta | tr '\n' '@')
+fasta_files=${fasta_files%?}
+#run clermontyping
+conda activate clermontyping
+bash clermonTyping.sh --fasta $fasta_files --name clermontyping_output
+mv clermontyping_output ../../results
+}
+
+#Run:
+run_clermontyping
