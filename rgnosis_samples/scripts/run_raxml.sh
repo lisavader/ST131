5401c41006c8ca1d808eb8d215f8bc4d62a755fb
Lisa Vader <lisa@mmvinder.nl>
2021-11-17 18:25:44 +0100

    add bootstrap values to tree

--- rgnosis_samples/scripts/run_raxml.sh
+++ rgnosis_samples/scripts/run_raxml.sh
@@ -6,4 +6,14 @@ conda activate raxml
 cd ../results/panaroo_output_all
 
 #run RAxML with CAT model of rate heterogeneity
+run_raxml(){
 raxmlHPC-PTHREADS -m GTRCAT -s core_gene_alignment.aln -n ml_tree -N 20 -T 8 -p 1901
+}
+
+#add bootstrap values
+draw_bootstrap_values(){
+cat RAxML_result.ml_tree.RUN.* > bootstrap_trees
+raxmlHPC -f b -m GTRCAT -t RAxML_bestTree.ml_tree -z bootstrap_trees -n bootstrap_tree
+}
+
+draw_bootstrap_values
