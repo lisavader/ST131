80277ea24997a0cfeec9e4ab2bf16d01ee84b71d
Lisa Vader <lisa@mmvinder.nl>
2021-12-21 15:26:01 +0100

    add code for running platon without parallelisation for calculating cpu time

--- benchmarking/binary_classifiers/scripts/run_platon.sh
+++ benchmarking/binary_classifiers/scripts/run_platon.sh
@@ -18,6 +18,19 @@ source /home/dla_mm/lvader/data/miniconda3/etc/profile.d/conda.sh
 conda activate platon
 
 run_platon(){
+cd ../../results/platon_predictions
+#check whether input directory exists
+[ ! -d ../../$1 ] && exit 1
+#run platon on all strains in input directory
+for strain in ../../$1/*.fasta
+do
+name=$(basename $strain .fasta)
+echo "Running platon on" $name
+platon --db ../../databases/platon/db --output $name --threads 8 $strain
+done
+}
+
+run_platon_parallel(){
 cd ../../results
 #check whether input directory exists
 #[ ! -d ../$1 ] && exit 1
