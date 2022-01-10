739f92b9165be52945fe26e22969fd4e9d905879
Lisa Vader <lisa@mmvinder.nl>
2022-01-10 13:56:34 +0100

    gzip raw reads in parallel

--- Ecoli_ncbi_download/scripts/get_reads_assemblies.sh
+++ Ecoli_ncbi_download/scripts/get_reads_assemblies.sh
@@ -5,14 +5,15 @@ mkdir ../results/trimmed_sra_files
 mkdir ../results/genomes
 
 #get GCA accession nr of benchmarking strains
+#For all strains in the list benchmark_strains.csv, it has been checked that they are not present in reference databases / training datasets of mlplasmids, PlaScope, Platon or RFPlasmid.
 cat ../results/benchmark_strains.csv | while read line; do
 	accession=$(echo $line | cut -d '_' -f2)
 
 	#copy raw reads
-	#raw_reads_files=$(ls /hpc/archive/dla_mm/jpaganini/recovering_ecoli_plasmids/2020_08_30_run_predictions/data/sra_files | grep $accession)
-	#for file in $raw_reads_files; do
-		#cp /hpc/archive/dla_mm/jpaganini/recovering_ecoli_plasmids/2020_08_30_run_predictions/data/sra_files/${file} ../results/raw_reads
-	#done
+	raw_reads_files=$(ls /hpc/archive/dla_mm/jpaganini/recovering_ecoli_plasmids/2020_08_30_run_predictions/data/sra_files | grep $accession)
+	for file in $raw_reads_files; do
+		cp /hpc/archive/dla_mm/jpaganini/recovering_ecoli_plasmids/2020_08_30_run_predictions/data/sra_files/${file} ../results/raw_reads
+	done
 
 	#copy trimmed reads
 	trimmed_reads_files=$(ls /hpc/dla_mm/jpaganini/data/ecoli_binary_classifier/2021_07_test_plasmid_ec/2021_07_28_prepare_dataset/data/trimmed_reads | grep $accession)
@@ -26,5 +27,14 @@ cat ../results/benchmark_strains.csv | while read line; do
 done
 
 #Gzip raw reads because bactofidia needs it
-#but takes a long time, so would rather have a different solution
-#gzip ../results/raw_reads/*.fastq
+mkdir gzip_scripts
+for sample in  ../results/raw_reads/*.fastq; do
+	name=$(basename $sample)
+	echo "#!/bin/bash 
+gzip ../${sample}" > gzip_scripts/${name}.sh 
+done
+cd gzip_scripts
+for job in $(ls *.sh); do
+	sbatch --mem 10G --time 00:30:00 $job
+done
+
