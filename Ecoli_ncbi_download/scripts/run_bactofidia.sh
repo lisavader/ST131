eb9679d7adc5f3142bb190ee30c0eaf0c7a686b5
Lisa Vader <lisa@mmvinder.nl>
2022-01-10 14:40:20 +0100

    change bactofidia location, add step for moving results

--- Ecoli_ncbi_download/scripts/run_bactofidia.sh
+++ Ecoli_ncbi_download/scripts/run_bactofidia.sh
@@ -3,15 +3,22 @@
 #SBATCH --mem=40G
 #SBATCH -c 8
 
+mkdir ../tools
+cd ../tools
+
+#clone bactofidia
+git clone https://gitlab.com/aschuerch/bactofidia.git bactofidia
+
 #run_bactofidia
-cd ../../../bactofidia
-ln -s ../ST131_repo/Ecoli_ncbi_download/results/raw_reads/* .
+cd bactofidia
+ln -s ../../results/raw_reads/* .
 bash ./bactofidia.sh *.fastq.gz
+#move results
+mkdir ../../results/shortread_assemblies_bactofidia
+mv *results ../../results/shortread_assemblies_bactofidia
 
-#move output to folder
-
-#change names back
-cd ../ST131_repo/Ecoli_ncbi_download/results/shortread_assemblies_bactofidia/scaffolds
+#change names back to original
+cd ../../results/shortread_assemblies_bactofidia/scaffolds
 for strain in $(ls *.fna); do 
 	new_name=$(echo $strain | sed 's/-/_/g')
 	mv $strain $new_name 
